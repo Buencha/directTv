6af9157f1b4f72be92acbd221356a34f3d5c62a8
Emmanuel Bravo <buenncha@gmail.com>
2018-05-28 12:19:48 -0300

    lunes

--- uruguay/js/main.js
+++ uruguay/js/main.js
@@ -8,14 +8,17 @@ $(".right ul li").click(function(event) {
 	var selected = event.target;
 	var chosenIndex = $(selected).attr("class");
 
-	if (chosenIndex == "caqueta") {
+	if (chosenIndex == "school") {
 		$(".slider")[0].slick.slickGoTo(0);
 	}
-	if (chosenIndex == "mitu") {
+	if (chosenIndex == "visor") {
 		$(".slider")[0].slick.slickGoTo(1);
 	}
-	if (chosenIndex == "pescadito") {
+	if (chosenIndex == "campanella") {
 		$(".slider")[0].slick.slickGoTo(2);
+	}
+	if (chosenIndex == "hands") {
+		$(".slider")[0].slick.slickGoTo(3);
 	}
 
 	$('html, body').animate({

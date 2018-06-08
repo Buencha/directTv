9e1badf4ba3599b4c10f3b7b8a5fe75ba017cc79
J.A.L <J.A.L>
2018-06-08 06:55:02 -0300

    new argentina design

--- argentina/js/main.js
+++ argentina/js/main.js
@@ -11,35 +11,54 @@ $(document).ready(function(){
   		 var firstH = $(".upperPart").first().css("height");
 
   		$(".campanella").css("height", firstH);
+  		$(".visor").css("height", firstH);
+  		$(".hands").css("height", firstH);
+  		$(".school").css("height", firstH);
 	},3500)
  
-	 
+	 // draw();
 	});
 
   
 
+function school() {
+	$(".slider")[0].slick.slickGoTo(0);
+	scrollDown();
+}
 
+function visor() {
+	$(".slider")[0].slick.slickGoTo(1);
+	scrollDown();
 
-$(".right ul li").click(function(event) {
-	
-	var selected = event.target;
-	var chosenIndex = $(selected).attr("class");
+}
 
-	if (chosenIndex == "school") {
-		$(".slider")[0].slick.slickGoTo(0);
-	}
-	if (chosenIndex == "visor") {
-		$(".slider")[0].slick.slickGoTo(1);
-	}
-	if (chosenIndex == "campanella") {
-		$(".slider")[0].slick.slickGoTo(2);
-	}
-	if (chosenIndex == "hands") {
-		$(".slider")[0].slick.slickGoTo(3);
-	}
+function campanella() {
+	$(".slider")[0].slick.slickGoTo(2);
+	scrollDown();
 
+}
+
+function hands() {
+	$(".slider")[0].slick.slickGoTo(3);
+	scrollDown();
+
+}
+
+function scrollDown() {
 	$('html, body').animate({
                     scrollTop: $(".slider").offset().top
                 }, 1000);
-});
+}
 
+function draw () {
+	var line = $('#line');
+	var div1 = $('.lower h4');
+	var div2 = $('.circulos');
+
+	var x1 = div1.offset().left + (div1.width()/2);
+	var y1 = div1.offset().top + (div1.height()/2);
+	var x2 = div2.offset().left + (div2.width()/2);
+	var y2 = div2.offset().top + (div2.height()/2);
+
+	line.attr('x1',x1).attr('y1',y1).attr('x2',x2).attr('y2',y2);
+}

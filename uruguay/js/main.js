e93e2267f9abc686bdf18d2d668b62924b655af1
Emmanuel Bravo <buenncha@gmail.com>
2018-06-11 11:36:46 -0300

    migrate

--- uruguay/js/main.js
+++ uruguay/js/main.js
@@ -1,28 +1,52 @@
 
 $(document).ready(function(){
   $('.slider').slick();
-});
+  
+  $('.upperPart').slick({
+  	dots: true,
+  	arrows:false
+  });
+  	
+	setTimeout(function(){
+  		 var firstH = $(".upperPart").first().css("height");
 
-$(".right ul li").click(function(event) {
-	
-	var selected = event.target;
-	var chosenIndex = $(selected).attr("class");
+  		$(".suelo").css("height", firstH);
+  		$(".futbol").css("height", firstH);
+  		$(".rugby").css("height", firstH);
+  		$(".school").css("height", firstH);
+	},3500)
+ 
+	 // draw();
+	});
 
-	if (chosenIndex == "school") {
-		$(".slider")[0].slick.slickGoTo(0);
-	}
-	if (chosenIndex == "visor") {
-		$(".slider")[0].slick.slickGoTo(1);
-	}
-	if (chosenIndex == "campanella") {
-		$(".slider")[0].slick.slickGoTo(2);
-	}
-	if (chosenIndex == "hands") {
-		$(".slider")[0].slick.slickGoTo(3);
-	}
+  
 
+function school() {
+	$(".slider")[0].slick.slickGoTo(1);
+	scrollDown();
+}
+
+function futbol() {
+	$(".slider")[0].slick.slickGoTo(2);
+	scrollDown();
+
+}
+
+function suelo() {
+	$(".slider")[0].slick.slickGoTo(3);
+	scrollDown();
+
+}
+
+function rugby() {
+	$(".slider")[0].slick.slickGoTo(4);
+	scrollDown();
+
+}
+
+function scrollDown() {
 	$('html, body').animate({
                     scrollTop: $(".slider").offset().top
                 }, 1000);
-});
+}
 

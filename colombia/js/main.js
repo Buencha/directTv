19691f4bd6044004d5c6ceb030ac1c1cd5faa51c
J.A.L <J.A.L>
2018-06-29 10:47:32 -0300

    adding colombia missing imgs

--- colombia/js/main.js
+++ colombia/js/main.js
@@ -1,25 +1,70 @@
 
+
 $(document).ready(function(){
   $('.slider').slick();
-});
-
-$(".right li").click(function(event) {
+  
+  $('.upperPart').slick({
+  	dots: true,
+  	arrows: true
+  });
+  	
+	setTimeout(function(){
+  		 var firstH = $(".upperPart").first().css("height");
+  		$(".caqueta").css("height", firstH);
+  		$(".mitu").css("height", firstH);
+  		$(".pescadito").css("height", firstH);
+	},3500)
+ 	 
+ 	bothSliders();
 	
-	var selected = event.target;
-	var chosenIndex = $(selected).attr("class");
+	});
 
-	if (chosenIndex == "caqueta") {
-		$(".slider")[0].slick.slickGoTo(0);
-	}
-	if (chosenIndex == "mitu") {
-		$(".slider")[0].slick.slickGoTo(1);
-	}
-	if (chosenIndex == "pescadito") {
-		$(".slider")[0].slick.slickGoTo(2);
-	}
+  
 
+function caqueta() {
+	$(".slider")[0].slick.slickGoTo(1);
+	scrollDown();
+}
+
+function mitu() {
+	$(".slider")[0].slick.slickGoTo(2);
+	scrollDown();
+
+}
+
+function pescadito() {
+	$(".slider")[0].slick.slickGoTo(3);
+	scrollDown();
+
+}
+
+
+function scrollDown() {
 	$('html, body').animate({
                     scrollTop: $(".slider").offset().top
                 }, 1000);
-});
+}
 
+function bothSliders() {
+	$(document).on( 
+		"click", ".slider .slick-next.slick-arrow",
+		function(e){
+		 	$(".upperPart .slick-next.slick-arrow").click();
+		}
+	);
+
+	$(document).on( 
+			"click", ".slider .slick-prev.slick-arrow",
+			function(e){
+			 	$(".upperPart .slick-prev.slick-arrow").click();
+			}
+		);
+
+
+	$('.upperPart').on('beforeChange', function(event, slick ,thisSlide, nextSlide){
+	  
+	  	$(".slider")[0].slick.slickGoTo(nextSlide);
+	  
+	 
+	});
+}
